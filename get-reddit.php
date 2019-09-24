3e680ba22b6feae360540892c637e5be219c1791
ylafaro0310 <y16mukurou@gmail.com>
2019-09-24 19:22:44 +0900

    Complete saving reddit article to json file

--- get-reddit.php
+++ get-reddit.php
@@ -2,21 +2,22 @@
 $url = 'https://www.reddit.com/r/programming/';
 $html = file_get_contents($url);
 $html = mb_convert_encoding($html,'UTF8','ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+
 $dom = new DOMDocument();
-libxml_use_internal_errors( true );
-$dom->loadHTML($html);
-libxml_clear_errors();
+@$dom->loadHTML($html);
 
 $xpath = new DOMXpath($dom);
-$titles_node = $xpath->query("//div[contains(@class,'rpBJOHq2PR60pnwJlUyP0')]/div/div/div/div/article/div/div/div/a/div/h3");
-$users_node = $xpath->query("//div[contains(@class,'rpBJOHq2PR60pnwJlUyP0')]/div/div/div/div/article/div/div/div/div/div/a");
-
-$titles = [];
-foreach($titles_node as $node){
-    $titles[] = $node->nodeValue;
-}
-$users = [];
-foreach($users_node as $node){
-    $users[] = $node->nodeValue;
+$articles_node = $xpath->query("//div[contains(@class,'rpBJOHq2PR60pnwJlUyP0')]/div/div/div/div/article/div");
+$articles = [];
+foreach($articles_node as $node){
+    $titles_node = $xpath->query("./div/div/a/div/h3",$node);
+    $article['title'] = $titles_node->item(0)->nodeValue;
+    $users_node = $xpath->query("./div/div/div/div/a",$node);
+    $article['user'] = $users_node->item(0)->nodeValue;    
+    $articles[] = $article;
 }
 
+$article_json = json_encode($articles,JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+
+//var_dump($article_json);
+file_put_contents('reddit_articles.json',$article_json);

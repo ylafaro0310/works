70827f098cec74b82cc04197d7da5627ac2ad3a6
ylafaro0310 <y16mukurou@gmail.com>
2019-09-27 09:55:10 +0900

    First commit

---
+++ dir-json.php
@@ -0,0 +1,23 @@
+<?php
+// Scan current dir or specified dir
+function scanDirectory($pathname){
+  if(!is_dir($pathname) || basename($pathname) === "." || basename($pathname) === ".."){
+    return basename($pathname);
+  }
+
+  $dirs = scandir($pathname);
+  //var_dump($dirs);
+  $result = [];
+  foreach($dirs as $dir){
+    $scanned_dir = scanDirectory($pathname . "/" . $dir);
+    if(is_string($scanned_dir)){
+      $result[] = $scanned_dir;
+    }else{
+      $result[$dir] = $scanned_dir;
+    }
+  }
+  return $result;
+}
+
+$json = json_encode(scanDirectory(realpath(".")),JSON_PRETTY_PRINT);
+print($json);

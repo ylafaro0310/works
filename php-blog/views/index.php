57be31842b4057ec86ae47e5203de42b8306e8b9
ylafaro0310 <y16mukurou@gmail.com>
2020-07-29 19:00:57 +0900

    Move views

---
+++ php-blog/views/index.php
@@ -0,0 +1,13 @@
+<header>
+    <link href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css" rel="stylesheet"/>
+</header>
+<body>
+    <div class="container">
+        <div class="box">
+            <h2>タイトル</h2>
+            <article>
+                ここに記事の要約が表示されます。
+            </article>
+        </div>
+    </div>
+</body>
---
+++ php-blog/views/login.php
@@ -0,0 +1,19 @@
+<header>
+    <link href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css" rel="stylesheet"/>
+</header>
+<body>
+    <div class="container">
+        <div class="field">
+            <div class="control">
+                <label class="label">ユーザ名</label>
+                <input class="input" type="text"/>
+            </div>
+        </div>
+        <div class="field">
+            <div class="control">
+                <label class="label">パスワード</label>
+                <input class="input" type="password"/>
+            </div>
+        </div>
+    </div>
+</body>

1ba15c8998d2e3b5ab8f901d234812da38af38aa
ylafaro0310 <y16mukurou@gmail.com>
2021-04-23 10:20:39 +0900

    Create CRUD

--- works/sql_alchemy/app.py
+++ works/sql_alchemy/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, make_response, jsonify, request
-from flaskr.database import init_db, db
-from flaskr.models import *
+from flaskr.database import init_db
+from flaskr.route import api
 
 def create_app():
     app = Flask(__name__)
@@ -9,6 +9,8 @@ def create_app():
     app.config['SQLALCHEMY_ECHO'] = False
     init_db(app)
     
+    app.register_blueprint(api)
+
     return app
 
 app = create_app()
@@ -16,25 +18,6 @@ app = create_app()
 @app.route('/')
 def hello():
     return 'Hello world!'
-
-@app.route('/users', methods=['GET'])
-def index():
-    user_schema = UserSchema(many=True)
-    return make_response(jsonify({
-        'code': 200,
-        'users': user_schema.dump(User.query.all())
-  }))
-
-@app.route('/users', methods=['POST'])
-def create():
-    name = "test"#request.form['name']
-    user = User(name=name)
-    db.session.add(user)
-    db.session.commit()
-    return make_response(jsonify({
-        'code': 200
-    }))
-
 
 if __name__ == '__main__':
     app.run("0.0.0.0", debug=True)
---
+++ works/sql_alchemy/flaskr/route.py
@@ -0,0 +1,62 @@
+from flask import Flask, Blueprint, make_response, jsonify, request, current_app
+from flaskr.database import db
+from flaskr.models import *
+
+api = Blueprint('api', __name__)
+
+@api.route('/users', methods=['GET'])
+def index():
+    user_schema = UserSchema(many=True)
+    return make_response(jsonify({
+        'code': 200,
+        'users': user_schema.dump(User.query.all())
+  }))
+
+@api.route('/users', methods=['POST'])
+def create():
+    if not request.is_json:
+        return make_response(jsonify({
+            'code': 400,
+            'message': 'Request data should be json.'
+        }))
+    name = request.json['name']
+    user = User(name=name)
+    db.session.add(user)
+    db.session.commit()
+    return make_response(jsonify({
+        'code': 200
+    }))
+
+@api.route('/users/<int:user_id>', methods=['GET'])
+def show(user_id):
+    user_schema = UserSchema()
+    user = User.query.filter_by(id=user_id).first()
+    return make_response(jsonify({
+        'code': 200,
+        'user': user_schema.dump(user)
+  }))
+
+@api.route('/users/<int:user_id>', methods=['PUT'])
+def update(user_id):
+    if not request.is_json:
+        return make_response(jsonify({
+            'code': 400,
+            'message': 'Request data should be json.'
+        }))
+    user = User.query.filter_by(id=user_id).first()
+    name = request.json['name']
+    user.name = name
+    db.session.add(user)
+    db.session.commit()
+    return make_response(jsonify({
+        'code': 200
+    }))
+
+@api.route('/users/<int:user_id>', methods=['DELETE'])
+def delete(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    db.session.delete(user)
+    db.session.commit()
+    return make_response(jsonify({
+        'code': 200
+    }))

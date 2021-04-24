64352e5ed3a2fecacaa5b97a826910a7bc5d315b
ylafaro0310 <y16mukurou@gmail.com>
2021-04-24 16:47:29 +0900

    Create test file

--- works/sql_alchemy/app.py
+++ works/sql_alchemy/app.py
@@ -1,23 +1,28 @@
 from flask import Flask, make_response, jsonify, request
+from dotenv import load_dotenv
+import os
 from flaskr.database import init_db
 from flaskr.route import api
 
+load_dotenv(override=True)
+
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://default:default@mysql/sql_alchemy?charset=utf8'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_ECHO'] = False
     init_db(app)
     
     app.register_blueprint(api)
 
+    @app.route('/',methods=['GET'])
+    def index():
+        return 'Hello world!'
+
     return app
 
 app = create_app()
 
-@app.route('/')
-def hello():
-    return 'Hello world!'
 
 if __name__ == '__main__':
     app.run("0.0.0.0", debug=True)

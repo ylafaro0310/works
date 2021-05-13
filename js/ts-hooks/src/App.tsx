997c8afb7e782055aac723eba926315f8dfb1782
ylafaro0310 <y16mukurou@gmail.com>
2021-05-13 21:22:42 +0900

    ts-hooks: Create redux code

--- js/ts-hooks/src/App.tsx
+++ js/ts-hooks/src/App.tsx
@@ -1,15 +1,51 @@
-import React, { useState } from 'react';
+import React, { Reducer, useState } from 'react';
+import { Provider, useDispatch, useSelector } from 'react-redux';
+import { AnyAction, createStore } from 'redux';
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+interface CounterState {
+  num: number
+}
+const initialState: CounterState = { num: 0 };
 
+const reducer = (state=initialState, action: AnyAction) => {
+  switch(action.type){
+    case "decrement":
+      return {...state, num: state.num - 1 };
+    case "increment":
+      return {...state, num: state.num + 1 };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+type RootState = ReturnType<typeof store.getState>
+type AppDispatch = typeof store.dispatch
+
+const App = ()=>{
   return (
     <div>
-      <p>You clicked {count} times</p>
-      <button onClick={()=>setCount(count+1)}>Click me</button>
+      <h2>ComponentUseReactRedux</h2>
+      <Provider store={store}>
+        <Click/>
+      </Provider>
     </div>
-  );
+  )
+}
+
+const Click = ()=>{
+  const num = useSelector((state: RootState)=>state.num);
+  const dispatch = useDispatch<AppDispatch>();
+  return (
+    <div>
+      <h3>Using useSelector, useDispatch</h3>
+      Number: {num}
+      <button onClick={()=>dispatch({type: "increment"})}>+</button>
+      <button onClick={()=>dispatch({type: "decrement"})}>-</button>
+    </div>
+  )
 }
 
 export default App;
